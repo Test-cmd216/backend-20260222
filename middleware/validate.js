956a998d4283c8d2538022642eb5628c036fdd8f
Test-cmd216 <fusionedge.mailbox@gmail.com>
2026-02-22 11:45:28 +0530

    Add middleware/validate.js

---
+++ middleware/validate.js
@@ -0,0 +1,10 @@
+const validate = (schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req.body, { abortEarly: false, stripUnknown: true });
+  if (error) {
+    const message = error.details.map(d => d.message.replace(/['"]/g, '')).join(', ');
+    return res.status(400).json({ success: false, message });
+  }
+  req.body = value;
+  next();
+};
+export default validate;
